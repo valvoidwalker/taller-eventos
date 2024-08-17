60cdee5ebd4cc54ac67ae759862f985bae41caa8
ValVoidwalker <valegomez3899@gmail.com>
2024-08-17 16:40:16 -0300

    Solucion respecto al evento y el div

--- eventos.js
+++ eventos.js
@@ -1,3 +1,9 @@
 document.getElementById('contenedor').addEventListener('click', function() {
     alert('Hola! Soy el div');
 });
+
+
+document.getElementById('miBoton').addEventListener('click', function(event) {
+    event.stopPropagation();
+    alert('¡Botón clickeado!');
+});
